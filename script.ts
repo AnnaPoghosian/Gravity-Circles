bc71c42feccf8a3878eca5d682e9fac72d5fadee
AnnaPoghosian <153907126+AnnaPoghosian@users.noreply.github.com>
2023-12-17 15:14:58 +0400

    script.ts

--- script.ts
+++ script.ts
@@ -1,13 +1,7 @@
-const canvas: HTMLCanvasElement = document.getElementById('gravityCanvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as HTMLElement;
+const canvas = document.getElementById('gravityCanvas') as HTMLCanvasElement;
+const canvasContext: CanvasRenderingContext2D | null = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const circles: Circle[] = [];
-let lastTime = 0;
-
-class Circle {
+ class Circle {
   x: number;
   y: number;
   radius: number;
@@ -21,82 +15,86 @@ class Circle {
     this.y = y;
     this.radius = radius;
     this.color = color;
-    this.dy = 0; 
-    this.gravity = 0.5; 
-    this.dampening = 0.9; 
+    this.dy = 0;
+    this.gravity = 0.5;
+    this.dampening = 0.9;
   }
 
-  draw(): void {
-    const gradient = ctx.createLinearGradient(
+  draw(canvasContext: CanvasRenderingContext2D): void {
+    const gradient = canvasContext.createLinearGradient(
       this.x - this.radius,
       this.y - this.radius,
       this.x + this.radius,
       this.y + this.radius
     );
 
-    gradient.addColorStop(0, "#fff"); 
-    gradient.addColorStop(1, this.color); 
+    gradient.addColorStop(0, "#fff");
+    gradient.addColorStop(1, this.color);
 
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = gradient;
-    ctx.fill();
-    ctx.closePath();
-    ctx.shadowInset = true;
-    ctx.shadowBlur = 30;
-    ctx.shadowColor = "#fff";
+    canvasContext.beginPath();
+    canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    canvasContext.fillStyle = gradient;
+    canvasContext.fill();
+    canvasContext.closePath();
+    canvasContext.shadowBlur = 30;
+    canvasContext.shadowColor = "#fff";
   }
 
-  update(): void {
-    // Apply gravity
+  update(canvas: HTMLCanvasElement): void {
     this.dy += this.gravity;
-
-    // Update position
     this.y += this.dy;
 
     if (this.y + this.radius > canvas.height) {
       this.y = canvas.height - this.radius;
       this.dy *= -this.dampening;
     }
-
-    this.draw();
   }
-}
-
-function tick(currentTime: number) {
-  const deltaTime = currentTime - lastTime;
-
-  // Update game elements using deltaTime
-  circles.forEach(circle => {
-    circle.update();
-  });
-
-  lastTime = currentTime;
-  requestAnimationFrame(tick);
-}
-
-function animate(): void {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  circles.forEach(circle => {
-    circle.update();
-  });
-
-  requestAnimationFrame(animate);
-}
-function spawnCircle(x: number, y: number): void {
-  const radius: number = 50;
-  const color: string = getRandomColor();
-  const circle: Circle = new Circle(x, y, radius, color);
-  circles.push(circle);
 }
 
 function getRandomColor(): string {
   const red: number = Math.floor(Math.random() * 256);
   const green: number = Math.floor(Math.random() * 256);
   const blue: number = Math.floor(Math.random() * 256);
-  const color: string = `rgb(${red}, ${green}, ${blue})`;
-  return color;
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const circlesList: Circle[] = [];
+let lastTime = 0;
+
+function tick(currentTime: number) {
+  const deltaTime = currentTime - lastTime;
+
+  circlesList.forEach((circle) => {
+    circle.update(canvas);
+  });
+
+  lastTime = currentTime;
+  requestAnimationFrame(() => tick(currentTime));
+}
+
+function spawnCircle(x: number, y: number): void {
+  const radius: number = 50;
+  const color: string = getRandomColor();
+  const circle: Circle = new Circle(x, y, radius, color);
+  circlesList.push(circle);
+}
+
+function animateCircles(
+  circlesList: Circle[],
+  canvasContext: CanvasRenderingContext2D,
+  canvas: HTMLCanvasElement
+): void {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+  circlesList.forEach((circle) => {
+    circle.update(canvas);
+    circle.draw(canvasContext);
+  });
+
+  requestAnimationFrame(() => animateCircles(circlesList, canvasContext, canvas));
 }
 
 canvas.addEventListener('click', (e: MouseEvent) => {
@@ -105,5 +103,5 @@ canvas.addEventListener('click', (e: MouseEvent) => {
 
 spawnCircle(canvas.width / 2, canvas.height / 2);
 
-animate();
+animateCircles(circlesList, canvasContext!, canvas);
 requestAnimationFrame(tick);
